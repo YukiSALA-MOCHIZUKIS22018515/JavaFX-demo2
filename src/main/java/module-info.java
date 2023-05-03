26620a1a88e0ba5167d1c0dac2acd5a78e69cda4
SALA-MOCHIZUKI Yuki <s22018515@A-F002082.salsa.univ-amu.fr>
2023-05-03 08:44:27 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.javafxdemo2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.javafxdemo2 to javafx.fxml;
+    exports com.example.javafxdemo2;
+}
